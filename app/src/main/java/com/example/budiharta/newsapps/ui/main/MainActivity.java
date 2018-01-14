99be3476eee523f550d0d4a4560a1247320c4a45
budiharta <budiharta_21@live.com>
2018-01-14 11:28:26 +0800

    adding recycler view

--- app/src/main/java/com/example/budiharta/newsapps/ui/main/MainActivity.java
+++ app/src/main/java/com/example/budiharta/newsapps/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.budiharta.newsapps;
+package com.example.budiharta.newsapps.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.budiharta.newsapps.R;
 
 public class MainActivity extends AppCompatActivity {
 
